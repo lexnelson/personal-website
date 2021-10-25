e2c2beab35c40378ef52793fd5d40c0de794efb2
Lex Nelson <lexinelson597@gmail.com>
2021-10-25 11:22:39 -0400

    added routes and basic components

---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+
+
+function Home(){
+    return(
+        <div>
+            <h2>Hello from home!</h2>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+
+function Navbar(){
+    return(
+        <div>
+            hello from Navbar
+        </div>
+    )
+}
+
+export default Navbar
